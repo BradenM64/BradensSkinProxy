28a2a6d79f0da95019557dff57edc16c7e7d78e8
Braden <bmorris0524@outlook.com>
2026-09-20 17:42:38 -0400

    Re-add proxy config options

--- server.js
+++ server.js
@@ -80,6 +80,6 @@ app.get("/api/minecraft/:username", async function (req, res) {
     }
 });
 
-app.listen(3000, "0.0.0.0", function () {
-    console.log("BradensSkinProxy running on port 3000");
+app.listen(config.proxy.port, config.proxy.hostname, function () {
+    console.log("BradensSkinProxy running on " + config.proxy.hostname + ":" + config.proxy.port);
 });
